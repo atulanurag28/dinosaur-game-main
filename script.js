f612ba652d23278eaa48151e77af926d97ab6c5b
Atul Anand <97800127+atulanurag28@users.noreply.github.com>
2024-05-21 11:20:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function play(){
+    document.getElementById("tree").setAttribute("class","cactus");
+    setTimeout(() => {
+        document.getElementById("tree1").style.visibility="visible";
+        document.getElementById("tree1").setAttribute("class","cactus");
+    }, 3000);
+
+    document.body.onkeyup=function(e){
+        if(e.code=='Space'){
+            document.getElementById("trex").setAttribute("class","jump");
+            setTimeout(() => {
+                document.getElementById("trex").removeAttribute("class");
+            }, 2000);
+        }
+    }
+
+    var id=setInterval(() => {
+        var a=document.getElementById("trex").offsetTop;
+        var b=document.getElementById("tree").offsetLeft;
+        var c=document.getElementById("tree1").offsetLeft;
+
+        if((a>=115 && a<=168)&&((b>=0 && b<=70)||(c>=0 && c<=70))){
+            document.getElementById("tree").style.animationPlayState="paused";
+            document.getElementById("tree1").style.animationPlayState="paused";
+            document.getElementById("trex").style.animationPlayState="paused";
+
+            alert("GAME OVER !!");
+            clearInterval(id);
+        }
+    }, 10);
+
+    document.getElementById("play").removeAttribute("onclick");
+}
+
+function replay(){
+    window.location.reload();
+}
+
+
